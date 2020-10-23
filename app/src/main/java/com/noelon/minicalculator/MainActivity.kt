9c8d81175818e5231f5e667e9fa79429ce530871
Noel-elon <noelnwaelugo@gmail.com>
2020-10-23 07:40:07 +0100

    next button

--- app/src/main/java/com/noelon/minicalculator/MainActivity.kt
+++ app/src/main/java/com/noelon/minicalculator/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.noelon.minicalculator
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        button_next.setOnClickListener {
+
+            //Todo: Incase you want to test your work, uncomment this intent to lead to your activity, but comment it back when you're done
+
+            //val intent = Intent(this, YourActivity::class.java)
+            //startActivity(intent)
+        }
     }
 }
